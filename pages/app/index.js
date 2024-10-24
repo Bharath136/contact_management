9f65d6b17ee9b5c45239d9ca7d92103576d34576
Bharath <bk3604073@gmail.com>
2024-10-24 12:58:10 +0530

    pushing

---
+++ pages/app/index.js
@@ -0,0 +1,13 @@
+// pages/app/index.js
+
+import React from 'react';
+
+const AppPage = () => {
+    return (
+        <div>
+            <h1>Welcome to the App Page</h1>
+        </div>
+    );
+};
+
+export default AppPage;
