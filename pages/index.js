ae6f081515787a08d8e01cfead5104c800ec9745
Bharath <bk3604073@gmail.com>
2024-10-24 13:07:15 +0530

    db added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,7 @@
-// pages/index.js
-import React from 'react';
-
-const Home = () => {
+export default function Home() {
     return (
         <div>
             <h1>Welcome to the Contact Management App!</h1>
         </div>
     );
-};
-
-export default Home;
+}
